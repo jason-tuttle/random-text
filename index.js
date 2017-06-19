ffee279e9e74c69c3664f65ba880cb7e258302b9
Jason Tuttle <jason@hearspotbark.com>
2017-06-19 15:32:48 -0400

    hard mode level up

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/lorem', function(req, res) {
   res.send(output);
 });
 
+app.get('/lorem/:count', function(req, res) {
+  res.send(loremIpsum({
+    "count": req.params.count,
+    units: 'paragraphs',
+    format: 'html'
+  }));
+});
+
 app.listen(3000, function() {
   console.log("Successfully started express application!");
 });
